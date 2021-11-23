c2ac46f46cbfc697a1d97555d7e1648a0125ec52
DaShaun <dashaun@dashaun.com>
2021-11-23 12:18:34 -0600

    Add unlisted plan

---
+++ acre_20/variables.tf
@@ -0,0 +1,17 @@
+variable "acre_sku" {
+  type    = string
+  default = "Enterprise_E100-20"
+}
+
+variable "azure_redis_enterprise_database_clustering_policy" {
+  type    = string
+  default = "OSSCluster"
+}
+
+variable "random_id" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
