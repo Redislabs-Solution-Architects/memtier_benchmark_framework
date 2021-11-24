2b144f75835754d023b1d5fd6e3ff7cf28af7b89
DaShaun <dashaun@dashaun.com>
2021-11-24 08:26:08 -0600

    refactored

--- module.tf
+++ module.tf
@@ -1,15 +1,38 @@
+module "ec2-memtier" {
+  source            = "./ec2-memtier"
+  security_group_id = module.elasticache.sg
+  ssh_key_name      = module.elasticache.key
+}
+
 module "elasticache" {
-  source = "./elasticache"
+  source     = "./elasticache"
+  aws_region = var.aws_region
+}
+
+module "rg" {
+  source = "./azure-resource-group"
+}
+
+module "memtier" {
+  source         = "./memtier"
+  random_id      = module.rg.random
+  resource_group = module.rg.resource_group
 }
 
 module "premium" {
-  source = "./premium"
+  source         = "./premium"
+  random_id      = module.rg.random
+  resource_group = module.rg.resource_group
 }
 
 module "acre" {
-  source = "./acre"
+  source         = "./acre"
+  random_id      = module.rg.random
+  resource_group = module.rg.resource_group
 }
 
 module "acref" {
-  source = "./acref"
+  source         = "./acref"
+  random_id      = module.rg.random
+  resource_group = module.rg.resource_group
 }
