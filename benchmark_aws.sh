56a218adcbf9d1a2ac727a35c3658f90473787fe
DaShaun <dashaun@redislabs.com>
2021-11-24 10:47:39 -0600

    use default pipeline

--- benchmark_aws.sh
+++ benchmark_aws.sh
@@ -9,4 +9,4 @@ export MEMTIER_HOST=$(terraform output -raw ec2-memtier | jq -r '.host')
 export MEMTIER_BIN=$(terraform output -raw ec2-memtier | jq -r '.program')
 
 echo "Executing Memtier Benchmark via SSH"
-ssh -o UserKnownHostsFile=/dev/null -i privateKey -o StrictHostKeyChecking=no "$MEMTIER_USER"@"$MEMTIER_HOST" "$MEMTIER_BIN -s $EC_HOST -p $EC_PORT --tls --tls-skip-verify --pipeline=1 -c 4 -t 12 --distinct-client-seed --hide-histogram --key-pattern=S:S --key-maximum=1000000 --test-time=30 --data-size=100 --ratio=1:1 --cluster-mode"
+ssh -o UserKnownHostsFile=/dev/null -i privateKey -o StrictHostKeyChecking=no "$MEMTIER_USER"@"$MEMTIER_HOST" "$MEMTIER_BIN -s $EC_HOST -p $EC_PORT --tls --tls-skip-verify -c 4 -t 12 --distinct-client-seed --hide-histogram --key-pattern=S:S --key-maximum=1000000 --test-time=30 --data-size=100 --ratio=1:1 --cluster-mode"
